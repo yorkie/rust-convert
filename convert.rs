18c318a644bbc3d3f60da1c108309750cafabaf7
Yazhong Liu <yorkiefixer@gmail.com>
2014-07-09 00:05:24 +0800

    update

--- convert.rs
+++ convert.rs
@@ -2,8 +2,18 @@
 #![crate_id = "convert#0.0.1"]
 #![crate_type = "lib"]
 
+extern crate collections;
+use collections::string::String;
 use std::str;
 
-pub fn string_to_str(raw: String) -> &str {
-  str::from_utf8(raw.as_bytes()).unwrap()
+#[inline]
+pub fn string_to_str<'a>(raw: &'a String) -> Option<&'a str> {
+  str::from_utf8((&raw).as_bytes())
+}
+
+#[test]
+fn test() {
+  let s1 = String::from_str("foobar");
+  let s2 = string_to_str(&s1).unwrap();
+  assert_eq!(s2, "foobar");
 }
